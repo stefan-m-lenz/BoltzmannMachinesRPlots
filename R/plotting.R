409f4a7bc2e0d93fc8ed539368aacb495f30e342
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2019-06-12 18:05:02 +0200

    Filtering evaluations

--- R/plotting.R
+++ R/plotting.R
@@ -1,6 +1,3 @@
-library(ggplot2)
-library(gridExtra)
-
 plotEvaluation <- function(monitor, evaluation = NULL) {
 
    if (length(monitor) == 0) {
@@ -12,11 +9,18 @@ plotEvaluation <- function(monitor, evaluation = NULL) {
       plotdata <- data.frame(Epoch = unlist(plotdata[, "epoch"]),
                              Value = unlist(plotdata[, "value"]),
                              Dataset = unlist(plotdata[, "datasetname"]),
-                             Evaluation = unlist(plotdata[, "evaluation"]))
+                             Evaluation = unlist(plotdata[, "evaluation"]),
+                             stringsAsFactors = FALSE)
+
       singleEvaluationPlot <- function(plotdata) {
          ggplot(data = plotdata, aes(x = Epoch, y = Value, color = Dataset)) +
             geom_line()
       }
+
+      if (!is.null(evaluation)) {
+         plotdata <- plotdata[plotdata$Evaluation %in% evaluation, ]
+      }
+
       plots <- lapply(split(plotdata, plotdata$Evaluation), singleEvaluationPlot)
       grid.arrange(grobs = plots)
    } else {
--- test/test.R
+++ test/test.R
@@ -10,3 +10,6 @@ result = monitored_fitrbm(barsandstripes(10L, 4L),
                           monitoring = juliaExpr("[monitorexactloglikelihood!, monitorreconstructionerror!]"))
 monitor = result[[1]]
 plotEvaluation(monitor)
+
+plotEvaluation(monitor, "reconstructionerror")
+plotEvaluation(monitor, c("reconstructionerror", "exactloglikelihood"))
