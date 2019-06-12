7740e78999f0d6b606a7d200c2519a93963197a0
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2019-06-12 17:06:34 +0200

    grid of plots

--- R/plotting.R
+++ R/plotting.R
@@ -1,4 +1,5 @@
-plotEvaluation <- function(monitor) {
+plotEvaluation <- function(monitor, evaluation = NULL) {
+
    if (length(monitor) == 0) {
       return()
    }
@@ -7,9 +8,14 @@ plotEvaluation <- function(monitor) {
       plotdata <- do.call(rbind, monitor)
       plotdata <- data.frame(Epoch = unlist(plotdata[, "epoch"]),
                              Value = unlist(plotdata[, "value"]),
-                             Dataset = unlist(plotdata[, "datasetname"]))
-      ggplot(data = plotdata, aes(x = Epoch, y = Value, color = Dataset)) +
-         geom_line()
+                             Dataset = unlist(plotdata[, "datasetname"]),
+                             Evaluation = unlist(plotdata[, "evaluation"]))
+      singleEvaluationPlot <- function(plotdata) {
+         ggplot(data = plotdata, aes(x = Epoch, y = Value, color = Dataset)) +
+            geom_line()
+      }
+      plots <- lapply(split(plotdata, plotdata$Evaluation), singleEvaluationPlot)
+      grid.arrange(grobs = plots)
    } else {
       # list of lists
    }
@@ -17,6 +23,7 @@ plotEvaluation <- function(monitor) {
 }
 
 
-result = monitored_fitrbm(barsandstripes(10L, 4L), monitoring = juliaExpr("monitorexactloglikelihood!"))
-monitor = result[[1]]
-plotEvaluation(monitor)
+# result = monitored_fitrbm(barsandstripes(10L, 4L),
+#                           monitoring = juliaExpr("[monitorexactloglikelihood!, monitorreconstructionerror!]"))
+# monitor = result[[1]]
+# plotEvaluation(monitor)
