e429f2bae441998b4f036d908abeab0d37bd560d
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2019-07-30 14:22:19 +0200

    Generalized plotting for dbms and nested monitoring results

--- R/plotting.R
+++ R/plotting.R
@@ -1,31 +1,49 @@
 plotEvaluation <- function(monitor, evaluation = NULL) {
 
-   if (length(monitor) == 0) {
+   if (!is.list(monitor) || length(monitor) == 0 || is.null(attr(monitor, "JLTYPE"))) {
       return()
    }
 
-   if (attr(monitor[[1]], "JLTYPE") == "MonitoringItem") {
-      plotdata <- do.call(rbind, monitor)
-      plotdata <- data.frame(Epoch = unlist(plotdata[, "epoch"]),
-                             Value = unlist(plotdata[, "value"]),
-                             Dataset = unlist(plotdata[, "datasetname"]),
-                             Evaluation = unlist(plotdata[, "evaluation"]),
-                             stringsAsFactors = FALSE)
-
-      singleEvaluationPlot <- function(plotdata) {
-         ggplot(data = plotdata, aes(x = Epoch, y = Value, color = Dataset)) +
-            geom_line()
-      }
-
-      if (!is.null(evaluation)) {
-         plotdata <- plotdata[plotdata$Evaluation %in% evaluation, ]
-      }
-
-      plots <- lapply(split(plotdata, plotdata$Evaluation), singleEvaluationPlot)
+   if (startsWith(attr(monitor, "JLTYPE"), "Array{MonitoringItem")) {
+      # plot different evaluations contained in the monitor in one grid
+      evaluations <- extractEvaluations(monitor, evaluation)
+      plots <- lapply(evaluations, singleEvaluationPlot)
       grid.arrange(grobs = plots)
-   } else {
-      # list of lists
+   } else if (startsWith(attr(monitor, "JLTYPE"), "Array{Array{MonitoringItem")) {
+      # plot all monitoring results from monitoring a DBM or DBN in a grid
+      evaluations <- unlist(lapply(monitor, function(m) {extractEvaluations(m, evaluation)}),
+                            recursive = FALSE)
+      plots <- lapply(evaluations, singleEvaluationPlot)
+      grid.arrange(grobs = plots)
+   } else if (is.list(monitor)) {
+      # this is, e.g., the case when monitor is bundled with a Boltzmann machines,
+      # as in the output of monitored_fitdbm
+      for (el in monitor) {
+         plotEvaluation(el, evaluation)
+      }
    }
-
 }
+
+
+singleEvaluationPlot <- function(plotdata) {
+   ggplot(data = plotdata, aes(x = Epoch, y = Value, color = Dataset)) +
+      geom_line()
+}
+
+
+# returns a list of data frames each contatining the data for one plot
+extractEvaluations <- function(monitor, evaluation) {
+   plotdata <- do.call(rbind, monitor)
+   plotdata <- data.frame(Epoch = unlist(plotdata[, "epoch"]),
+                          Value = unlist(plotdata[, "value"]),
+                          Dataset = unlist(plotdata[, "datasetname"]),
+                          Evaluation = unlist(plotdata[, "evaluation"]),
+                          stringsAsFactors = FALSE)
+   if (!is.null(evaluation)) {
+      plotdata <- plotdata[plotdata$Evaluation %in% evaluation, ]
+   }
+   evaluations <- split(plotdata, plotdata$Evaluation)
+   evaluations
+}
+
 
--- test/test.R
+++ test/test.R
@@ -2,14 +2,17 @@
 
 library(JuliaConnectoR)
 juliaUsing("BoltzmannMachines")
-result = monitored_fitrbm(barsandstripes(10L, 4L),
+result <- monitored_fitrbm(barsandstripes(10L, 4L),
                           monitoring = juliaExpr("monitorexactloglikelihood!"))
-monitor = result[[1]]
-plotEvaluation(monitor)
-result = monitored_fitrbm(barsandstripes(10L, 4L),
+plotEvaluation(result)
+result <- monitored_fitrbm(barsandstripes(10L, 4L),
                           monitoring = juliaExpr("[monitorexactloglikelihood!, monitorreconstructionerror!]"))
-monitor = result[[1]]
-plotEvaluation(monitor)
+plotEvaluation(result)
 
-plotEvaluation(monitor, "reconstructionerror")
-plotEvaluation(monitor, c("reconstructionerror", "exactloglikelihood"))
+result <- monitored_fitdbm(barsandstripes(100L, 4L),
+                          monitoring = juliaExpr("monitorexactloglikelihood!"),
+                          monitoringpretraining = juliaExpr("monitorreconstructionerror!"))
+plotEvaluation(result)
+
+plotEvaluation(result, "reconstructionerror")
+plotEvaluation(result, c("reconstructionerror", "exactloglikelihood"))
