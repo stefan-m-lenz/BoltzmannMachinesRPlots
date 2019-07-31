07e6f889f1156d8dba4693e32d9adfff0d40ea46
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2019-07-31 15:36:14 +0200

    plotMonitoring() works also directly with the returned list from dsBoltzmannMachinesClient

--- R/plotting.R
+++ R/plotting.R
@@ -1,28 +1,31 @@
 plotMonitoring <- function(monitor, evaluation = NULL) {
 
-   if (!is.list(monitor) || length(monitor) == 0 || is.null(attr(monitor, "JLTYPE"))) {
-      return()
+   if (!is.list(monitor) || length(monitor) == 0) {
+      return(invisible())
    }
 
-   if (startsWith(attr(monitor, "JLTYPE"), "Array{MonitoringItem")) {
-      # plot different evaluations contained in the monitor in one grid
-      evaluations <- extractEvaluations(monitor, evaluation)
-      plots <- lapply(evaluations, singleEvaluationPlot)
-      grid.arrange(grobs = plots)
-   } else if (startsWith(attr(monitor, "JLTYPE"), "Array{Array{MonitoringItem")) {
-      # plot all monitoring results from monitoring a DBM or DBN in a grid
-      evaluations <- unlist(lapply(monitor, function(m) {extractEvaluations(m, evaluation)}),
-                            recursive = FALSE)
-      plots <- lapply(evaluations, singleEvaluationPlot)
-      grid.arrange(grobs = plots)
-   } else if (is.list(monitor)) {
-      # this is, e.g., the case when monitor is bundled with a Boltzmann machines,
-      # as in the output of monitored_fitdbm
-      for (el in monitor) {
-         plotMonitoring(el, evaluation)
+   if (!is.null(attr(monitor, "JLTYPE"))) {
+      if (startsWith(attr(monitor, "JLTYPE"), "Array{MonitoringItem")) {
+         # plot different evaluations contained in the monitor in one grid
+         evaluations <- extractEvaluations(monitor, evaluation)
+         plots <- lapply(evaluations, singleEvaluationPlot)
+         grid.arrange(grobs = plots)
+         return(invisible())
+      } else if (startsWith(attr(monitor, "JLTYPE"), "Array{Array{MonitoringItem")) {
+         # plot all monitoring results from monitoring a DBM or DBN in a grid
+         evaluations <- unlist(lapply(monitor, function(m) {extractEvaluations(m, evaluation)}),
+                               recursive = FALSE)
+         plots <- lapply(evaluations, singleEvaluationPlot)
+         grid.arrange(grobs = plots)
+         return(invisible())
       }
    }
+
+   for (el in monitor) {
+      plotMonitoring(el, evaluation)
+   }
 }
+
 
 plottitles = list(
    "reconstructionerror" = "Mean reconstruction error",
@@ -35,14 +38,19 @@ plottitles = list(
    "cordiff" = "L²-difference between correlation matrices \nof generated and original data",
    "freeenergy" = "Free energy")
 
+
 singleEvaluationPlot <- function(plotdata) {
+
    ggplot(data = plotdata, aes(x = Epoch, y = Value, color = Dataset)) +
       geom_line() + ggtitle(plottitles[[plotdata$Evaluation[1]]])
+   #if (isaisplot(plotdata))
+   #geom_ribbon(aes(ymin = level - 1, ymax = level + 1)
 }
 
 
 # returns a list of data frames each contatining the data for one plot
 extractEvaluations <- function(monitor, evaluation) {
+
    plotdata <- do.call(rbind, monitor)
    plotdata <- data.frame(Epoch = unlist(plotdata[, "epoch"]),
                           Value = unlist(plotdata[, "value"]),
@@ -57,3 +65,15 @@ extractEvaluations <- function(monitor, evaluation) {
 }
 
 
+aisprecision <- function(logr, aissd, sdrange = 1.0) {
+   t <- sdrange * aissd * exp(-logr)
+   if (1 - t <= 0) {# prevent domainerror
+      diffbottom <- -Inf
+   } else {
+      diffbottom <- log(1 - t)
+   }
+
+   difftop <- log(1 + t)
+
+   c(diffbottom, difftop)
+}
